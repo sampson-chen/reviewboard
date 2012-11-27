7d0a013118879bd793898a41d909a29820049e87
sampson.chen@gmail.com <sampson.chen@gmail.com>
2012-11-26 22:12:11 -0500

    Removed xml_review_ui_extension to separate review

--- xml_review_ui_extension/xml_review_ui_extension/admin_urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-
-from xml_review_ui_extension.extension import XMLReviewUIExtension
-
-
-urlpatterns = patterns('xml_review_ui_extension.views',
-    url(r'^$', 'configure'),
-)
