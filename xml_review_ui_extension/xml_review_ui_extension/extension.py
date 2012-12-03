135c5b216ab0a01df98f793b2e2a25e376f1af85
sampson.chen@gmail.com <sampson.chen@gmail.com>
2012-12-02 23:57:40 -0500

    Added XMLMimetype Handler for XML Thumbnails

--- xml_review_ui_extension/xml_review_ui_extension/extension.py
+++ xml_review_ui_extension/xml_review_ui_extension/extension.py
@@ -1,11 +1,15 @@
 # xml_review_ui_extension Extension for Review Board.
 from reviewboard.extensions.base import Extension
-from reviewboard.extensions.hooks import ReviewUIHook, URLHook
+from reviewboard.extensions.hooks import ReviewUIHook, \
+                                         FileAttachmentThumbnailHook, \
+                                         URLHook
 
 from XMLReviewUI import XMLReviewUI
+from XMLThumbnail import XMLMimetype
 
 
 class XMLReviewUIExtension(Extension):
     def __init__(self, *args, **kwargs):
         super(XMLReviewUIExtension, self).__init__(*args, **kwargs)
         self.reviewui_hook = ReviewUIHook(self, [XMLReviewUI])
+        self.thumbnail_hook = FileAttachmentThumbnailHook(self, [XMLMimetype])
